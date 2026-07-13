cd0bc9967f50108d101c2e6c653626cf56fb6225
aleenadominic2003 <aleenadominic16@gmail.com>
2026-07-13 18:29:23 +0530

    Navigation and home page done

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,23 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import Home from './components/Home'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+     <BrowserRouter>
      
+      <Routes>
+
+        <Route path='/' element={<Home/>} />
+       
+
+      </Routes>
+     </BrowserRouter>
     </>
   )
 }
